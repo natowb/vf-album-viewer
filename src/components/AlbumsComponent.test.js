ccf31e567f35c61afc8a873819ba41b9d960f4c8
Nathan <nwbarnes@yahoo.com>
2020-10-05 11:48:06 +1300

    album component test initial commit

--- src/components/AlbumsComponent.test.js
+++ src/components/AlbumsComponent.test.js
@@ -1,1 +1,42 @@
 import React from "react";
+import { shallow } from "enzyme";
+import AlbumsComponent from "./AlbumsComponent";
+
+let wrapper;
+let onSelect = jest.fn();
+beforeEach(() => {
+  const albums = [
+    { id: 1, userId: 1, title: "Hello World" },
+    { id: 2, userId: 1, title: "Hello World 2" },
+    { id: 3, userId: 1, title: "Hello World 3" },
+  ];
+  const users = [{ id: 1, name: "John Doe" }];
+
+  wrapper = shallow(
+    <AlbumsComponent albums={albums} users={users} onSelect={onSelect} />
+  );
+});
+
+describe("Albums Component Testing", () => {
+  it("should render list of albums", () => {
+    expect(wrapper.find(".album-list")).toHaveLength(1);
+    expect(wrapper.find(".album-item")).toHaveLength(3);
+  });
+
+  it("should render correct text on album-item", () => {
+    expect(wrapper.find(".album-item")).toHaveLength(3);
+    expect(
+      wrapper.contains(
+        <li key="1" className=".album-itemm">
+          <p>Hello World</p>
+          <span>John Doe</span>
+        </li>
+      )
+    );
+  });
+
+  it("should call onSelect when item is clicked", () => {
+    wrapper.find(".album-item").at(0).simulate("click");
+    expect(onSelect).toHaveBeenCalledTimes(1);
+  });
+});
