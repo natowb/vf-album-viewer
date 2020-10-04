728f37d52e9f8ab200127f271eb7a3c07614e249
Nathan <nwbarnes@yahoo.com>
2020-10-05 12:12:27 +1300

    photos component initial commit

--- src/components/PhotosComponent.js
+++ src/components/PhotosComponent.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+const PhotosComponent = ({ photos }) => {
+  const [full, setFull] = useState(null);
 
-const PhotosComponent = () => {
-  return <div></div>;
+  return (
+    <div>
+      {full && <img className="fullscreen-img" src={full.url} />}
+      <ul className="photo-list">
+        {photos &&
+          photos.map((photo) => (
+            <li
+              key={photo.id}
+              className="photo-item"
+              onClick={() => {
+                setFull(photo);
+              }}
+            >
+              <img key={photo.id} src={photo.thumbnailUrl} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PhotosComponent;
